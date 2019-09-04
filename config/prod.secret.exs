e5732ba7308ac4dda99a559d001223496b9b6e82
Lukas <niederbergerlukas@gmail.com>
2019-09-03 21:16:41 -0700

    Fix error in prod config

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -7,7 +7,7 @@ database_url =
     For example: ecto://USER:PASS@HOST/DATABASE
     """
 
-config :onedotnine, Onedotnine.Repo,
+config :ex_zkb, ExZkb.Pathfinder.Repo,
   # ssl: true,
   url: database_url,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
